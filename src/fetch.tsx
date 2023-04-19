125fd7c13389eb3e98c5990c5332a7b22137db59
nsh58 <georgia6954@gmail.com>
2023-04-19 11:51:43 +0900

    card実装

--- src/fetch.tsx
+++ src/fetch.tsx
@@ -20,7 +20,7 @@ export async function getTopTracks(): Promise<any> {
 
 export async function getUserSavedAlbum(): Promise<any> {
   return (await fetchWebApi(
-    'v1/me/albums?limit=50', 'GET'
+    'v1/me/albums?limit=5', 'GET'
   )).items;
 }
 
@@ -33,16 +33,30 @@ console.log(userSavedAlbums);
 export default function UserSavedAlbums() {
   return (
     <div>
-        <ul>
-          {userSavedAlbums.map((userSavedAlbum: any) => {
-            return (
-              <li key={userSavedAlbum.added_at}>
-                {userSavedAlbum.added_at}
-                {userSavedAlbum.album.name}
-                </li>
-            )
-          })}
-        </ul>
+      {userSavedAlbums.map((userSavedAlbum: any) => {
+        return (
+          <div className="ui card">
+            <div className="image">
+              <img src={userSavedAlbum.album.images[0].url} />
+            </div>
+            <div className="content">
+              <a className="header">Kristy</a>
+              <div className="meta">
+                <span className="date">{userSavedAlbum.added_at}</span>
+              </div>
+              <div className="description">
+                Kristy is an art director living in New York.
+              </div>
+            </div>
+            <div className="extra content">
+              <a>
+                <i className="user icon"></i>
+                22 Friends
+              </a>
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
